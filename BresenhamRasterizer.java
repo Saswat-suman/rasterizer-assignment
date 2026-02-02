734b1018296cb3b24d4bec320248c4ba7a404d85
Annada Behera <annada.behera@hotmail.com>
2026-02-02 11:33:58 +0530

    Added sample points to the code.

--- BresenhamRasterizer.java
+++ BresenhamRasterizer.java
@@ -1,4 +1,8 @@
 import java.util.ArrayList;
 
 class BresenhamRasterizer implements LineRasterizer {
+    public Point[] rasterize(Point p1, Point p2) {
+        return new Point[]{ new Point(0,0), new Point(2,3) };
+    }
+
 }
